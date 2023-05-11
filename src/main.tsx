57e85f338be9d51f754c71c532349868538c7319
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-05-11 15:48:36 +0700

    Config jQuery and import JS for bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 import "./index.scss";
 
